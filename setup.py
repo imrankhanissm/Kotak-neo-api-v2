6f1041eeeb82b302c98dd6edf075afe729a94a1a
Imran <Imrankhanissm@gmail.com>
2025-12-17 23:15:44 +0530

    fork version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME = "neo-api-client"
-VERSION = "2.0.0-imran-1.0.0"
+VERSION = "2.0.0+imran.1"
 # To install the library, run the following
 #
 # python setup.py install
